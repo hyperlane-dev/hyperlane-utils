005187a7deb69011798fcc9d13c5f09f75fbb8d0
尤雨东 <root@ltpp.vip>
2025-05-28 22:29:55 +0800

    feat: v0.3.0

--- src/lib.rs
+++ src/lib.rs
@@ -6,19 +6,6 @@ pub use json::*;
 pub use thread::*;
 pub use utf8::*;
 
-pub use ahash;
-pub use futures;
-pub use lombok_macros::*;
-pub use num_cpus;
-pub use once_cell;
-pub use serde;
-pub use serde_json;
-pub use serde_urlencoded;
-pub use serde_xml_rs;
-pub use simd_json;
-pub use twox_hash;
-pub use urlencoding;
-
 pub use bin_encode_decode::*;
 pub use chunkify::*;
 pub use clonelicious::*;
@@ -31,6 +18,20 @@ pub use hyperlane_log::*;
 pub use recoverable_spawn::*;
 pub use recoverable_thread_pool::*;
 pub use server_manager::*;
+
+pub use ahash;
+pub use futures;
+pub use log;
+pub use lombok_macros::*;
+pub use num_cpus;
+pub use once_cell;
+pub use serde;
+pub use serde_json;
+pub use serde_urlencoded;
+pub use serde_xml_rs;
+pub use simd_json;
+pub use twox_hash;
+pub use urlencoding;
 
 pub(crate) use serde::Serialize;
 pub(crate) use serde::de::DeserializeOwned;
