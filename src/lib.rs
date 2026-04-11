3f8fbb1c50cb6a1600be8aa5a3e2faee1a89f74f
尤雨东 <root@ltpp.vip>
2026-04-11 11:01:57 +0800

    feat: v27.0.3

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,11 @@
 pub use {
     ahash, base64, bin_encode_decode::*, bytemuck_derive, chrono, chunkify::*, clonelicious::*,
     color_output::*, compare_version::*, dotenvy, file_operation::*, future_fn::*, futures, hex,
-    hot_restart::*, http_request::*, hyperlane_broadcast::*, hyperlane_log::*, hyperlane_macros::*,
+    hot_restart::*, hyperlane_broadcast::*, hyperlane_log::*, hyperlane_macros::*,
     hyperlane_plugin_websocket::*, instrument_level::*, jsonwebtoken, jwt_service::*, log,
     lombok_macros::*, md5, num_cpus, once_cell, recoverable_spawn::*, recoverable_thread_pool::*,
-    redis, regex, rust_decimal, sea_orm, serde_urlencoded, serde_with, serde_xml_rs, serde_yaml,
-    server_manager::*, sha2, simd_json, snafu, sqlx, std_macro_extensions::*, sysinfo, tracing_log,
-    tracing_subscriber, twox_hash, url, urlencoding, utoipa, utoipa_rapidoc, utoipa_swagger_ui,
-    uuid,
+    redis, regex, reqwest, rust_decimal, sea_orm, serde_urlencoded, serde_with, serde_xml_rs,
+    serde_yaml, server_manager::*, sha2, simd_json, snafu, sqlx, std_macro_extensions::*, sysinfo,
+    tracing_log, tracing_subscriber, twox_hash, url, urlencoding, utoipa, utoipa_rapidoc,
+    utoipa_swagger_ui, uuid,
 };
