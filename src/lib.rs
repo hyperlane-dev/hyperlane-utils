ce4eb9a6ad563896707af68917ab2f7d75a5a2ba
尤雨东 <root@ltpp.vip>
2026-04-17 21:58:37 +0800

    feat: v27.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@
 //! A library providing utils for hyperlane.
 
 pub use {
-    ahash, base64, bin_encode_decode::*, bytemuck_derive, chrono, chunkify::*, clonelicious::*,
-    color_output::*, compare_version::*, dotenvy, file_operation::*, future_fn::*, futures, hex,
-    hot_restart::*, hyperlane_broadcast::*, hyperlane_log::*, hyperlane_macros::*,
-    hyperlane_plugin_websocket::*, instrument_level::*, jsonwebtoken, jwt_service::*, log,
-    lombok_macros::*, md5, num_cpus, once_cell, recoverable_spawn::*, recoverable_thread_pool::*,
-    redis, regex, reqwest, rust_decimal, sea_orm, serde_urlencoded, serde_with, serde_xml_rs,
-    serde_yaml, server_manager::*, sha2, simd_json, snafu, sqlx, std_macro_extensions::*, sysinfo,
-    tracing_log, tracing_subscriber, twox_hash, url, urlencoding, utoipa, utoipa_rapidoc,
-    utoipa_swagger_ui, uuid,
+    aes, ahash, base64, bin_encode_decode::*, bytemuck_derive, chrono, chunkify::*, cipher,
+    clonelicious::*, color_output::*, compare_version::*, dotenvy, ed25519_dalek,
+    file_operation::*, future_fn::*, futures, hex, hot_restart::*, hyperlane_broadcast::*,
+    hyperlane_log::*, hyperlane_macros::*, hyperlane_plugin_websocket::*, instrument_level::*,
+    jsonwebtoken, jwt_service::*, log, lombok_macros::*, md5, num_cpus, once_cell,
+    recoverable_spawn::*, recoverable_thread_pool::*, redis, regex, reqwest, rsa, rust_decimal,
+    sea_orm, serde_urlencoded, serde_with, serde_xml_rs, serde_yaml, server_manager::*, sha2,
+    simd_json, snafu, sqlx, std_macro_extensions::*, sysinfo, tracing_log, tracing_subscriber,
+    twox_hash, url, urlencoding, utoipa, utoipa_rapidoc, utoipa_swagger_ui, uuid,
 };
